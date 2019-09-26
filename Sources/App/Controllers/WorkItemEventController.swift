beebb06f663ddfc8e555cfa1ce44d23e649add43
Zachary Ullevig <zullevig@bignerdranch.com>
2019-09-26 16:21:18 -0400

    Separated out controllers to own files

---
+++ Sources/App/Controllers/WorkItemEventController.swift
@@ -0,0 +1,36 @@
+import Vapor
+
+final class WorkItemEventController {
+    // API fetch all request example
+    func list(_ request: Request) throws -> Future<[WorkItemEvent]> {
+        let models = WorkItemEvent.query(on: request).all()
+        return models
+    }
+    
+    // API fetch item by ID request example
+    func get(_ request: Request) throws -> Future<WorkItemEvent> {
+        let modelID: Int = try request.parameters.next(Int.self)
+        let model = WorkItemEvent.find(modelID, on: request).unwrap(or: NotFound())
+        return model
+    }
+    
+    // API create item request example
+    func create(_ request: Request) throws -> Future<WorkItemEvent> {
+        let model = try request.content.decode(WorkItemEvent.self)
+        return model.create(on: request)
+    }
+    
+    // API update item request example
+    func update(_ request: Request) throws -> Future<WorkItemEvent> {
+        let model = try request.content.decode(WorkItemEvent.self)
+        return model.update(on: request)
+    }
+    
+    // API delete item request example
+    func delete(_ request: Request) throws -> Future<WorkItemEvent> {
+        let modelID: Int = try request.parameters.next(Int.self)
+        let model = WorkItemEvent.find(modelID, on: request).unwrap(or: NotFound())
+        return model.delete(on: request)
+    }
+}
+
